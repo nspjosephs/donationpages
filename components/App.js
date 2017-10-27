7d785ea334f386290a64117ccdd00a9f3b440198
nspjosephstewart <joseph.s@nationalschoolproject.com>
2017-10-27 15:29:35 -0700

    Changed some things

--- components/App.js
+++ components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {run,SpreedlyExpress} from 'bloom';
 
+run();
 export default class App extends React.Component {
+
+  collectPayment() {
+    SpreedlyExpress.onPaymentMethod((token,paymentMethod) => {
+      Bloomerang.CreditCard.spreedlyToken(token);
+    })
+  }
+
   render() {
     return (
       <div>
         <p>H3ll0 W0rld</p>
+        <button onclick={this.collectPayment.bind(this)}>Click me!</button>
       </div>
     )
   }
---
+++ components/bloom.js
@@ -0,0 +1,27 @@
+var run = () => {
+
+  if (window.ActiveXObject) {
+    Bloomerang.BROWSER_UNSUPPORTED = true;
+  }
+
+  if (!Boomerang.SpreedlyScriptLoaded) {
+    Bloomerang.Util.load('https://core.spreedly.com/iframe/express-2.min.js',
+    () => (SpreedlyExpress != undefined),
+    () => {
+      SpreedlyExpress.onInit(() => {
+        console.log("Spreedly Initilized...");
+      });
+      Bloomerang.initSpreedly = () => {
+        SpreedlyExpress.init('OqOMv1ksjPtXEYHtCYsVXzEpCbR'. { 'company_name':'National School Project' });
+      }
+      Bloomerang.initSpreedly();
+    });
+    Bloomerang.SpreedlyScriptLoaded = true;
+  }
+
+}
+
+export {
+  run,
+  SpreedlyExpress
+}
